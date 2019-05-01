5e6e84e3d52c2be87b82fe1ce4da4b64dfcb7679
Lucas Werner <lwerner27@gmail.com>
2019-05-01 15:41:36 -0500

    added folder for components with CustomNav comp.

---
+++ client/src/components/CustomNav.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import {
+	Navbar,
+	NavbarBrand,
+	NavbarToggler,
+	Collapse,
+	Nav,
+	NavItem,
+	NavLink
+} from "reactstrap";
+
+export default class CustomNav extends Component {
+	constructor(props) {
+		super(props);
+
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+			isOpen: false
+		};
+	}
+	toggle() {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
+	render() {
+		return (
+			<div>
+				<Navbar color="dark" dark expand="md">
+					<NavbarBrand href="/">My Fit</NavbarBrand>
+					<NavbarToggler onClick={this.toggle} />
+					<Collapse isOpen={this.state.isOpen} navbar>
+						<Nav className="ml-auto" navbar>
+							<NavItem>
+								<NavLink href="https://github.com/reactstrap/reactstrap">
+									GitHub
+								</NavLink>
+							</NavItem>
+						</Nav>
+					</Collapse>
+				</Navbar>
+			</div>
+		);
+	}
+}
