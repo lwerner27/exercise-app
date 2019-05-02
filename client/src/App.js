367a55e9350467bc35d6c99ece11b2f958920977
Lucas Werner <lwerner27@gmail.com>
2019-05-02 09:52:28 -0500

    Changed to Class Component and added method for showing modal.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import CustomNav from "./components/CustomNav";
+import LoginModal from "./components/LoginModal";
 
-function App() {
-	return (
-		<div className="App">
-			<CustomNav />
-		</div>
-	);
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			modal: false
+		};
+
+		this.toggle = this.toggle.bind(this);
+	}
+
+	toggle() {
+		this.setState(prevState => ({
+			modal: !prevState.modal
+		}));
+	}
+	render() {
+		let { modal } = this.state;
+		return (
+			<div>
+				<CustomNav toggleLogin={this.toggle} />
+				<LoginModal modal={modal} toggle={this.toggle} />
+			</div>
+		);
+	}
 }
-
-export default App;
