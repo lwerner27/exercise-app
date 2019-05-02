38e8da54cd03e57f79ba6c40d9b44d20e9ddeaff
Lucas Werner <lwerner27@gmail.com>
2019-05-02 11:07:40 -0500

    Added a "Home" page component.

---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+
+export default class Home extends Component {
+	render() {
+		return (
+			<div>
+				<Container>
+					<Row>
+						<Col>
+							<h1>This is the homepage.</h1>
+						</Col>
+					</Row>
+				</Container>
+			</div>
+		);
+	}
+}
