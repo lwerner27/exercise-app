f1873f2b290f7333136387a6987b4a10b30cfa6a
Lucas Werner <lwerner27@gmail.com>
2019-05-03 14:35:33 -0500

    Basic setup for state of the page.

--- client/src/pages/AddWorkout.js
+++ client/src/pages/AddWorkout.js
@@ -2,7 +2,40 @@ import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
 
 export default class AddWorkout extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			date: "",
+			exercises: []
+		};
+		this.compareDates = this.compareDates.bind(this);
+	}
+
+	compareDates(date, data) {
+		if (date === data.date) {
+			console.log("The dates match.");
+			this.setState(data);
+		} else {
+			console.log("The dates do not match.");
+			localStorage.removeItem("storedData");
+			this.setState({ date: date });
+		}
+	}
+
+	componentDidMount() {
+		let ls = localStorage;
+		let todaysDate = getDate();
+		let storedData = JSON.parse(ls.getItem("storedData"));
+
+		if (storedData) {
+			this.compareDates(todaysDate, storedData);
+		} else {
+			this.setState({ date: todaysDate });
+		}
+	}
+
 	render() {
+		console.log(this.state);
 		return (
 			<Container>
 				<Row>
@@ -13,4 +46,10 @@ export default class AddWorkout extends Component {
 			</Container>
 		);
 	}
+}
+
+function getDate() {
+	let date = new Date();
+	let today = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+	return today;
 }
