16a4773160190388959dedf6713b731569677596
Lucas Werner <lwerner27@gmail.com>
2019-05-03 12:14:46 -0500

    Added basics of the AddWorkout page.

---
+++ client/src/pages/AddWorkout.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+
+export default class AddWorkout extends Component {
+	render() {
+		return (
+			<Container>
+				<Row>
+					<Col>
+						<h1>This is the addWorkout page.</h1>
+					</Col>
+				</Row>
+			</Container>
+		);
+	}
+}
