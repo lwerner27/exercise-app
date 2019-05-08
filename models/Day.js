f60a24ccbe35b6b0d40fcaddca53fb3f7f68897c
Lucas Werner <lwerner27@gmail.com>
2019-05-08 15:21:15 -0500

    Updated some of the fields.

--- models/Day.js
+++ models/Day.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const daySchema = new Schema({
-	userId: { type: Schema.Types.ObjectId },
-	date: { type: Date, default: Date.now },
+	userId: { type: Schema.Types.ObjectId, ref: "User" },
+	dbDate: { type: Date, default: Date.now },
+	clientData: String,
 	exercises: [{ type: Schema.Types.ObjectId, ref: "Exercise" }]
 });
 
