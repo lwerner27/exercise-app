8530a23778ebba75f38e475343c082041e7b0c09
Lucas Werner <lwerner27@gmail.com>
2019-05-17 15:49:43 -0500

    Finalized setup of user model.

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// we create a user schema
+let userSchema = new Schema(
+	{
+		fullName: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		email: {
+			type: String,
+			required: true,
+			trim: true,
+			unique: true,
+			lowercase: true
+		},
+		password: {
+			type: String,
+			required: true
+		},
+		createdAt: {
+			type: Date,
+			required: false
+		},
+		updatedAt: {
+			type: Number,
+			required: false
+		},
+		days: [{ type: Schema.Types.ObjectId, ref: "Day" }]
+	},
+	{ runSettersOnQuery: true }
+);
+
+userSchema.pre("save", function(next) {
+	this.email = this.email.toLowerCase();
+
+	var currentDate = new Date().getTime();
+	this.updatedAt = currentDate;
+	if (!this.created_at) {
+		this.createdAt = currentDate;
+	}
+	next();
+});
+
+var User = mongoose.model("User", userSchema);
+
+module.exports = User;
