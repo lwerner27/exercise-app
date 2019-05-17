a3d40f0f7423395cfe449e1e9c784290be523f2d
Lucas Werner <lwerner27@gmail.com>
2019-05-17 11:49:10 -0500

    Configured environment variables and initialized firebase.

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const firebase = require("firebase");
 // const mongoose = require("mongoose");
 const routes = require("./routes");
-const admin = require("firebase-admin");
 
-admin.initializeApp({
-	credential: admin.credential.applicationDefault()
-});
+const firebaseConfig = {
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: "ljw-exercise-app.firebaseapp.com",
+	databaseURL: "https://ljw-exercise-app.firebaseio.com",
+	projectId: "ljw-exercise-app",
+	storageBucket: "ljw-exercise-app.appspot.com",
+	messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const PORT = process.env.PORT || 8080;
 const app = express();
