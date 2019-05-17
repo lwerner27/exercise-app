f3b13ffaf28c7b6781299a8270006bea182de368
Lucas Werner <lwerner27@gmail.com>
2019-05-17 12:21:28 -0500

    Removed firebase configuration from the server.

--- server.js
+++ server.js
@@ -1,21 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const firebase = require("firebase");
 // const mongoose = require("mongoose");
 const routes = require("./routes");
-
-const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: "ljw-exercise-app.firebaseapp.com",
-	databaseURL: "https://ljw-exercise-app.firebaseio.com",
-	projectId: "ljw-exercise-app",
-	storageBucket: "ljw-exercise-app.appspot.com",
-	messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
-	appId: process.env.FIREBASE_APP_ID
-};
-
-firebase.initializeApp(firebaseConfig);
 
 const PORT = process.env.PORT || 8080;
 const app = express();
