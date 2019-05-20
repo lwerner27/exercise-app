ceed1c5197782b81dbef831759fd71cf9117263a
Lucas Werner <lwerner27@gmail.com>
2019-05-20 10:41:20 -0500

    Added a login and logout routes.

--- routes/auth.js
+++ routes/auth.js
@@ -31,4 +31,39 @@ router.post("/register", (req, res) => {
 		});
 });
 
+router.post("/login", (req, res) => {
+	let { email, password } = req.body;
+	User.findOne({ email: email }, "username email password")
+		.then(userData => {
+			let passwordCheck = bcrypt.compareSync(password, userData.password);
+			if (passwordCheck) {
+				// Using bcrypt to check the password agains the hashed password in the database.
+				req.session.user = {
+					email: userData.email,
+					username: userData.username,
+					id: userData._id
+				};
+				// saving some user's data into user's session
+				req.session.user.expires = new Date(
+					// session expires in 3 days
+					Date.now() + 3 * 24 * 3600 * 1000
+				);
+				return res.status(200).json({ msg: "You are logged in." });
+			} else {
+				return res.status(401).json({ msg: "Invalid username or password." });
+			}
+		})
+		.catch(err => {
+			console.log(err);
+			return res
+				.status(400)
+				.json({ msg: "There was a problem loggin in please try again later." });
+		});
+});
+
+router.all("/logout", (req, res) => {
+	req.session.destroy();
+	res.status(200).json({ msg: "You have logged out." });
+});
+
 module.exports = router;
