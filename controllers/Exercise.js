c9925c2b70b5f2ac22589fed7de13b94fc29f491
Lucas Werner <lwerner27@gmail.com>
2019-05-24 14:16:33 -0500

    Now correctly saves exercises and associates them with the correct day.

--- controllers/Exercise.js
+++ controllers/Exercise.js
@@ -3,14 +3,41 @@ const Day = require("../models/Day");
 
 module.exports = {
 	addExercise: function(req, res) {
-		console.log(req.session.dayId);
 		let newExercise = new Exercise(req.body);
 
-		newExercise.save().catch(err => {
-			console.log(err);
-			res.status(400).json({
-				msg: "There was a problem saving your exercise please try again later."
+		newExercise
+			.save()
+			.then(exercise => {
+				Day.findById(req.session.dayId)
+					.then(day => {
+						day.exercises.push(exercise._id);
+						day
+							.save()
+							.then(day => {
+								return res.status(201).json(exercise);
+							})
+							.catch(err => {
+								console.log(err);
+								return res.status(400).json({
+									msg:
+										"There was a problem updating the day in the database please try again later."
+								});
+							});
+					})
+					.catch(err => {
+						console.log(err);
+						return res.status(400).json({
+							msg:
+								"There was a problem find the day in the database, please try again later."
+						});
+					});
+			})
+			.catch(err => {
+				console.log(err);
+				return res.status(400).json({
+					msg:
+						"There was a problem saving your exercise please try again later."
+				});
 			});
-		});
 	}
 };
