5dbad5ba043c29bffedf6ff1b230426f364c6a94
Lucas Werner <lwerner27@gmail.com>
2019-05-24 10:43:57 -0500

    Updated model to expire after 30 days.

--- models/Day.js
+++ models/Day.js
@@ -3,7 +3,11 @@ const Schema = mongoose.Schema;
 
 const daySchema = new Schema({
 	userId: { type: Schema.Types.ObjectId, ref: "User" },
-	dbDate: { type: Date, default: Date.now },
+	expiresAt: {
+		type: Date,
+		index: { expires: "30 days" },
+		default: Date.now
+	},
 	clientDate: { type: String, required: true },
 	exercises: [{ type: Schema.Types.ObjectId, ref: "Exercise" }]
 });
