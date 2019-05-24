92b173d9269b4ee3f912790aec5fff615b089aba
Lucas Werner <lwerner27@gmail.com>
2019-05-24 12:23:56 -0500

    Reverted addExercise function to its most basic form.

--- controllers/Exercise.js
+++ controllers/Exercise.js
@@ -1,20 +1,16 @@
 const Exercise = require("../models/Exercise");
+const Day = require("../models/Day");
 
 module.exports = {
 	addExercise: function(req, res) {
+		console.log(req.session.dayId);
 		let newExercise = new Exercise(req.body);
 
-		newExercise
-			.save()
-			.then(exercise => {
-				return res.status(201).json(exercise);
-			})
-			.catch(err => {
-				console.log(err);
-				res.status(400).json({
-					msg:
-						"There was a problem saving your exercise please try again later."
-				});
+		newExercise.save().catch(err => {
+			console.log(err);
+			res.status(400).json({
+				msg: "There was a problem saving your exercise please try again later."
 			});
+		});
 	}
 };
